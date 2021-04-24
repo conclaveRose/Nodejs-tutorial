870c1fa3c4b0a819f282315320c063dc45e5a948
conclaveRose <conclaveyohan.l@gmail.com>
2021-04-24 23:08:41 +0900

    두번째 커밋

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const express = require('express')
 const app = express();
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const bodyParser = require('body-parser'); //고객이 입력한 정보는 post요청에 의해 서버로 req.body객체안에 담겨져 전달되고 이것을 parsing하기 위해 필요한 패키지라고 보면 된다. 
+const ejs = require('ejs'); //ejs template engine을 사용한다. 
 const mongoose = require('mongoose');
 require('dotenv').config({ path: '.env' })
 
@@ -28,7 +28,7 @@ app.listen(port, () => {
 
 
 
-//DB setting 
+//DB setting  
 mongoose.set('useNewUrlParser', 'true');
 mongoose.set('useCreateIndex', 'true');
 mongoose.set('useUnifiedTopology', 'true');
--- routes/home.js
+++ routes/home.js
@@ -2,11 +2,23 @@
 
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User'); //user스키마 모델을 가져온다.
 
 
-// index 페이지로 랜더링 
 router.get('/', (req, res) => {
-    res.render('home/index');
+    res.send('hello World 좋은 하루 보내세요~~ ')
 })
+
+router.post('/', (req, res) => {
+    //회원 가입 할 때 필요한 정보들을 client에서 가져오면 
+    //그것들을 데이터베이스에 넣어준다.
+    User.create(req.body, (err, user) => {
+        if (err) {
+            return res.json(err);
+        }
+        res.send('성공적으로 가입완료');
+    })
+})
+
 
 module.exports = router;
